c673b3394db7a228a578dc9f0c7ffc42b5c75dec
Code-Smith-Craft <khudoyberganov98@gmail.com>
2025-01-10 21:04:55 +0500

    framework

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 
-$app = new Application();
+require_once __DIR__ . '/vendor/autoload.php';
+
+$app = new \app\core\Application();
 
 $app->router->get('/', function () {
     return "Hello world";
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'macos/csc.framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bd94017f9b840aa62095a086c2d2e74303ba3f97',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'macos/csc.framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bd94017f9b840aa62095a086c2d2e74303ba3f97',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
