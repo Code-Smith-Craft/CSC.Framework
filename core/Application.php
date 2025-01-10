d71c8092ea35424cab2e169eb456dda5db491384
Code-Smith-Craft <khudoyberganov98@gmail.com>
2025-01-10 22:01:19 +0500

    New

--- core/Application.php
+++ core/Application.php
@@ -5,9 +5,14 @@ namespace app\core;
 class Application
 {
     public Router $router;
-
+    public Request $request;
     public function __construct()
     {
         $this->router = new Router();
+    }
+
+    public function run()
+    {
+        $this->router->resolve();
     }
 }
